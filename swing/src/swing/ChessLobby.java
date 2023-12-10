4dbf17f809bbc321de006fbd617d3027a9427f2e
TerryKim2 <137304324+TerryKim2@users.noreply.github.com>
2023-12-10 11:08:37 +0900

    remove hard button

--- swing/src/swing/ChessLobby.java
+++ swing/src/swing/ChessLobby.java
@@ -13,7 +13,6 @@ public class ChessLobby extends JFrame {
     private JButton exitGameButton;
     private JButton easyButton;
     private JButton normalButton;
-    private JButton hardButton;
     private JButton bButton;
     private JButton wButton;
     private Image backgroundImage;
@@ -47,11 +46,6 @@ public class ChessLobby extends JFrame {
         textLabel4.setFont(new Font("times", Font.BOLD, 50));
         textLabel4.setHorizontalAlignment(JLabel.CENTER);
         textLabel4.setForeground(Color.white);
-
-        JLabel textLabel5 = new JLabel("HARD");
-        textLabel5.setFont(new Font("times", Font.BOLD, 50));
-        textLabel5.setHorizontalAlignment(JLabel.CENTER);
-        textLabel5.setForeground(Color.white);
 
         JLabel textLabel6 = new JLabel("WHITE");
         textLabel6.setFont(new Font("times", Font.BOLD, 50));
@@ -108,7 +102,7 @@ public class ChessLobby extends JFrame {
         exitGameButton.setVisible(true);
 
         easyButton = new JButton();
-        easyButton.setBounds(710, 256, 500, 150);
+        easyButton.setBounds(710, 403, 500, 150);
         easyButton.setIcon(img);
         easyButton.setFont(new Font("times", Font.BOLD, 50));
         easyButton.setForeground(Color.WHITE);
@@ -120,7 +114,7 @@ public class ChessLobby extends JFrame {
         easyButton.setVisible(false);
 
         normalButton = new JButton();
-        normalButton.setBounds(710, 506, 500, 150);
+        normalButton.setBounds(710, 726, 500, 150);
         normalButton.setIcon(img);
         normalButton.setFont(new Font("times", Font.BOLD, 50));
         normalButton.setForeground(Color.WHITE);
@@ -131,18 +125,6 @@ public class ChessLobby extends JFrame {
         normalButton.add(textLabel4, BorderLayout.CENTER);
         normalButton.setVisible(false);
 
-        hardButton = new JButton();
-        hardButton.setBounds(710, 756, 500, 150);
-        hardButton.setIcon(img);
-        hardButton.setFont(new Font("times", Font.BOLD, 50));
-        hardButton.setForeground(Color.WHITE);
-        hardButton.setBorderPainted(true);
-        hardButton.setContentAreaFilled(false);
-        hardButton.setFocusPainted(false);
-        hardButton.setLayout(new BorderLayout());
-        hardButton.add(textLabel5, BorderLayout.CENTER);
-        hardButton.setVisible(false);
-
         chess = new JLabel("Chess Game");
         chess.setFont(new Font("times", Font.BOLD, 80));
         chess.setForeground(Color.WHITE);
@@ -150,7 +132,7 @@ public class ChessLobby extends JFrame {
         chess.setHorizontalAlignment(JLabel.CENTER);
 
         select = new JLabel("Select Difficulty");
-        select.setBounds(100, 506, 600, 150);
+        select.setBounds(100, 564, 600, 150);
         select.setFont(new Font("times", Font.BOLD, 70));
         select.setForeground(Color.getHSBColor(51, 100, 100));
         select.setVisible(false);
@@ -195,7 +177,6 @@ public class ChessLobby extends JFrame {
         contentPane1.add(exitGameButton);
         contentPane1.add(easyButton);
         contentPane1.add(normalButton);
-        contentPane1.add(hardButton);
         contentPane1.add(wButton);
         contentPane1.add(bButton);
 
@@ -209,7 +190,6 @@ public class ChessLobby extends JFrame {
                 exitGameButton.setVisible(false);
                 easyButton.setVisible(true);
                 normalButton.setVisible(true);
-                hardButton.setVisible(true);
                 select.setVisible(true);
             }
         });
@@ -241,7 +221,6 @@ public class ChessLobby extends JFrame {
                 exitGameButton.setVisible(false);
                 easyButton.setVisible(false);
                 normalButton.setVisible(false);
-                hardButton.setVisible(false);
                 select.setVisible(false);
                 wButton.setVisible(true);
                 bButton.setVisible(true);
@@ -256,27 +235,13 @@ public class ChessLobby extends JFrame {
                 exitGameButton.setVisible(false);
                 easyButton.setVisible(false);
                 normalButton.setVisible(false);
-                hardButton.setVisible(false);
                 select.setVisible(false);
                 wButton.setVisible(true);
                 bButton.setVisible(true);
                 easy = false;
             }
         });
-        //hard
-        hardButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                startGameButton.setVisible(false);
-                exitGameButton.setVisible(false);
-                easyButton.setVisible(false);
-                normalButton.setVisible(false);
-                hardButton.setVisible(false);
-                select.setVisible(false);
-                wButton.setVisible(true);
-                bButton.setVisible(true);
-            }
-        });
+
         //select player color as black
         bButton.addActionListener(new ActionListener() {
             @Override
