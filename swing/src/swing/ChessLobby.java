4525b775c6e6065cf0f5ca54595ec8276cb9fb60
Sofiia Rudenka <sofiiarudenka@Sofiias-MacBook-Pro.local>
2023-12-11 10:54:15 -0500

    modified chess game title sizing a bit

--- swing/src/swing/ChessLobby.java
+++ swing/src/swing/ChessLobby.java
@@ -128,7 +128,7 @@ public class ChessLobby extends JFrame {
         chess = new JLabel("Chess Game");
         chess.setFont(new Font("times", Font.BOLD, 80));
         chess.setForeground(Color.WHITE);
-        chess.setBounds(470, 35, 500, 150);
+        chess.setBounds(450, 35, 600, 150);
         chess.setHorizontalAlignment(JLabel.CENTER);
 
         select = new JLabel("Select Difficulty");
